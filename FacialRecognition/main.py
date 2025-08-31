555d025068326a7bd88cfb1f7adaf7d5db67296d
Aditya <107750640+xdityagr@users.noreply.github.com>
2025-09-01 01:12:49 +0530

    Update main.py

--- FacialRecognition/main.py
+++ FacialRecognition/main.py
@@ -1,12 +1,18 @@
+""" 
+SHIELD Facial Recognition Module - Main Source 
+Developed by Aditya Gaur
+Github : @xdityagr 
+Email  : adityagaur.home@gmail.com 
 
+"""
 import enroll,recognition,cv2
 print(cv2.__version__)
 recognition.load_facial_encodings_and_names_from_memory()
-#spreadsheet.mark_all_absent()
 recognition.run_recognition()
 # name=input("Name:")
 #enroll.enroll_via_camera(name)
 #enroll.encoding_of_enrolled_person("Name","Path")
+
 
 
 
